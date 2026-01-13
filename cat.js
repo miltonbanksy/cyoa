ee878f3883509ec40fb827773ba4ee777bc9349b
miltonbanksy <miltonbanksy@gmail.com>
2026-01-14 01:04:54 +0900

    add initial HTML structure, CSS styles, and JavaScript for cat character

---
+++ cat.js
@@ -0,0 +1,9 @@
+const catMoves = ["run", "sneak", "balance", "climb", "swim"];
+const catAttacks = ["claw", "bite"];
+const catSpeak = ["hiss", "purr", "screech", "meow"];
+
+const seasons = ["spring", "summer", "autumn", "winter"];
+
+const locations = [
+    {name: "wasteground", description: "• Open ground, concrete and scattered weeds"}
+];
